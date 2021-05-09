868a61bc4c4aaddbd4c6dd888a3da472b049290c
CalmCharacter <scholarlynewton@gmail.com>
2021-05-09 20:56:13 +0800

    echo server sockets

---
+++ Client.java
@@ -0,0 +1,37 @@
+package sockets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class Client {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+	
+		Socket socket = new Socket ("127.0.0.1",9090); 	//127.0.0.1 is the local host address. socket port 9090 as long as greater than 1024
+		
+		PrintWriter pw = new PrintWriter (socket.getOutputStream(), true);	//printwriter exports content to the location socket.getOutputStream, flush is true means it sends as soon as you run the printline.
+		BufferedReader key = new BufferedReader (new InputStreamReader(System.in)); 
+		
+		String input = "";
+		while (!input.equals("exit")) {
+			System.out.println("\ninput your message to the server");
+			System.out.print("> ");
+			input = key.readLine();
+			pw.println(input);
+			}
+		
+		System.out.println("you are now disconnected.");
+		
+		
+		socket.close();
+		pw.close();
+		System.exit(0);
+	
+	
+	}
+}
---
+++ Server.java
@@ -0,0 +1,45 @@
+package sockets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+	try {	
+		ServerSocket ss = new ServerSocket (9090); //server socket with port 9090
+		System.out.println("waiting for the client...");
+		
+		Socket client = ss.accept(); 	//a socket object is created to connect server to client.
+		System.out.println("you are now connected to the client!");
+		
+		BufferedReader fb = new BufferedReader (new InputStreamReader(client.getInputStream())); //it reads what it receives from client to the server
+		
+		String received = "";
+		while (!received.equals("exit")) {
+		received = fb.readLine();
+		
+		System.out.println("\nResponse to the client");
+		System.out.print("> "+received+"\n");
+		
+	
+		}
+		
+		System.out.println("Disconnected from the client.");
+		ss.close();
+		fb.close();	
+		client.close();
+		
+		} catch (Exception e) {		//use try catch due to null.pointer.exception in line 23.
+			
+		}
+	
+	
+	}
+	}	
+
+
